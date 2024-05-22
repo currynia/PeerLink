74cd497f2c9bfb1e9f28d837678581d3b0b9a6d0
Wen Rui <teewenrui31@gmail.com>
2024-05-22 14:47:36 +0800

    Initial commit

---
+++ src/test/java/peerlink/peerlink/PeerlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package peerlink.peerlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
