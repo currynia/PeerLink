9fc50e96567f6107ebd03d38f35987a900b52968
Wen Rui <teewenrui31@gmail.com>
2024-05-24 21:06:15 +0800

    add: backend for registering user to db

--- src/main/java/peerlink/peerlink/PeerlinkApplication.java
+++ src/main/java/peerlink/peerlink/PeerlinkApplication.java
@@ -1,21 +1,45 @@
 package peerlink.peerlink;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+import org.springframework.web.servlet.resource.ResourceResolverChain;
+
+import java.util.List;
 
 @SpringBootApplication
 
 public class PeerlinkApplication implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PeerlinkApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PeerlinkApplication.class, args);
+    }
 
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/").setViewName("index.html");
-	}
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index.html");
+    }
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/").resourceChain(false)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    public Resource resolveResource(
+                            HttpServletRequest request,
+                            String requestPath,
+                            List<? extends Resource> locations,
+                            ResourceResolverChain chain) {
+                        Resource resource = super.resolveResource(request, requestPath, locations, chain);
+                        return resource == null
+                                ? super.resolveResource(request, "/index.html", locations, chain)
+                                : resource;
+                    }
+                });
+    }
 }
--- src/main/java/peerlink/peerlink/controllers/RegisterController.java
+++ src/main/java/peerlink/peerlink/controllers/RegisterController.java
@@ -1,13 +1,25 @@
 package peerlink.peerlink.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import peerlink.peerlink.db.model.User;
 import peerlink.peerlink.db.repository.UserRepository;
 
 @RestController
-public class RegisterController {
+class RegisterController {
     @Autowired
     UserRepository userRepo;
+
+    @PostMapping(value = "/api/register", consumes = "application/json")
+    private String register(@RequestBody User user) {
+        try {
+            userRepo.save(user);
+            return "success";
+        } catch (IllegalArgumentException e) {
+            return "fail";
+        }
+    }
 
 }
